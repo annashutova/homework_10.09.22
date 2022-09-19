bf81c514d02aea6c214f618f8c739ef5448cf0a2
annashutova <annashut@inbox.ru>
2022-09-19 19:38:29 +0300

    Исправлен файл task_5.py

--- 12.09.22/task_5.py
+++ 12.09.22/task_5.py
@@ -5,10 +5,10 @@ for line in lines:
     num_up = 0
     num_low = 0
     for symb in line:
-        if symb.isupper:
+        if symb.isupper():
             num_up += 1
         else:
             num_low += 1
     if num_up > num_low:
         upper += 1
-print('Заглавных букв больше в {0}% строк'.format(round(cnt_lines/upper*100, 1)))
+print('Заглавных букв больше в {0}% строк'.format(upper/cnt_lines*100))
