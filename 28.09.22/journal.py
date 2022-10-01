174f0ef59f969f5f107e8f1bc79974cf10998fef
annashutova <annashut@inbox.ru>
2022-10-01 13:03:59 +0300

    Переделано

--- 28.09.22/journal.py
+++ 28.09.22/journal.py
@@ -3,11 +3,11 @@ from datetime import date, timedelta
 def make_journal(notes, year, month, day):
     file = open('captains_journal.txt', 'a')
     current_date = date(year, month, day)
-    line = '{0}: {1}\n'.format(notes[0], str(current_date))
+    line = '{0}: {1}\n'.format(str(current_date), notes[0])
     file.write(line)
     for i in range(1, len(notes)):
         next_date = current_date + timedelta(days=1)
-        line = '{0}: {1}\n'.format(notes[i], str(next_date))
+        line = '{0}: {1}\n'.format(str(next_date), notes[i])
         current_date = next_date
         file.write(line)
     file.close() 
