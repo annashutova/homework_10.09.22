7a9adeb37191949ea5d122bd2f84cb849b3f9909
annashutova <annashut@inbox.ru>
2022-10-07 16:47:18 +0300

    Чуть-чуть поправила домашку

--- 19.09.22/date_validator.py
+++ 19.09.22/date_validator.py
@@ -1,12 +1,27 @@
-def valid_date(date):
+MONTH30 = (4, 6, 9, 11)
+MONTH31 = (1, 3, 5, 7, 8, 10, 12)
+FEBRUARY = 2
+FEB_DAY = 29
+
+
+def valid_date(date: str) -> bool:
+    """
+    Checks if the input date is valid.
+
+    Args:
+        date : str - day, month and year of the date.
+
+    Return:
+        bool - true if date is valid, else false.
+    """
     date = date.split('.')
     try:
         day, month, year = int(date[0]), int(date[1]), int(date[2])
     except Exception:
         return 'Input format: DD.MM.YYYY'
     leap_year = (year % 4 == 0 and year % 100 != 0) or year % 400 == 0
-    leap_date = leap_year and day == 29 and month == 2
-    normal_30 = month in (4, 6, 9, 11) and day in range(1, 31)
-    normal_31 = month in (1, 3, 5, 7, 8, 10, 12) and day in range(1, 32)
-    february = not (leap_year) and month == 2 and day in range(1, 29)
-    return leap_date or normal_30 or normal_31 or february
+    leap_date = leap_year and day == FEB_DAY and month == FEBRUARY
+    normal30 = month in MONTH30 and day in range(1, 31)
+    normal31 = month in MONTH31 and day in range(1, 32)
+    february = not (leap_year) and month == FEBRUARY and day in range(1, 29)
+    return leap_date or normal30 or normal31 or february
--- 28.09.22/journal.py
+++ 28.09.22/journal.py
@@ -1,17 +1,22 @@
 from datetime import date, timedelta
 
 
-def make_journal(notes, year, month, day):
-    file = open('captains_journal.txt', 'a')
-    current_date = date(year, month, day)
-    line = '{0}: {1}\n'.format(str(current_date), notes[0])
-    file.write(line)
-    for i in range(1, len(notes)):
-        next_date = current_date + timedelta(days=1)
-        line = '{0}: {1}\n'.format(str(next_date), notes[i])
-        current_date = next_date
-        file.write(line)
-    file.close()
+def make_journal(notes: list, year: int, month: int, day: int) -> None:
+    """
+    Writes lines of the list into file with date marks.
 
-
-make_journal(['first note', 'second note', 'third note', 'fourth note'], 2022, 9, 29)
+    Args:
+        notes : list - list of the capitan's notes.
+        year : int - initial year .
+        month : int - initial month.
+        day : int - initial day.
+    """
+    with open('captains_journal.txt', 'a') as journal:
+        current_date = date(year, month, day)
+        line = '{0}: {1}\n'.format(str(current_date), notes[0])
+        journal.write(line)
+        for index in range(1, len(notes)):
+            next_date = current_date + timedelta(days=1)
+            line = '{0}: {1}\n'.format(str(next_date), notes[index])
+            current_date = next_date
+            journal.write(line)
