3d81e6e21934e1c35d277065d53836af0c46bad9
annashutova <annashut@inbox.ru>
2022-10-24 21:58:54 +0300

    Все исправлено по flake8

--- 19.09.22/test_date.py
+++ 19.09.22/test_date.py
@@ -5,7 +5,7 @@ import pytest
 tests = [('12.03.2021', True), ('29.02.2005', False), ('31.10.22', True)]
 
 
-@pytest.mark.parametrize('inp, result', tests)
-def test_date(inp, result):
+@pytest.mark.parametrize('inp, answer', tests)
+def test_date(inp, answer):
     """Tests valid_date function."""
-    assert date_validator.valid_date(inp) == result
+    assert date_validator.valid_date(inp) == answer
